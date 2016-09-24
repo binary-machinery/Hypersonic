36be8aaf1e543d439c0b8f841a886ef277d81444
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-24 21:12:25 +0300

    Cell, Grid and utility calculation

--- src/Player.java
+++ src/Player.java
@@ -8,6 +8,7 @@ class Boomer {
     int bombsAvailable;
     int explosionRange;
 
+    @Override
     public String toString() {
         return "Boomer {" + x + "," + y + "," + bombsAvailable + "," + explosionRange + "}";
     }
@@ -20,14 +21,67 @@ class Bomb {
     int roundsLeft;
     int explosionRange;
 
+    @Override
     public String toString() {
         return "Bomb {" + x + "," + y + "," + roundsLeft + "," + explosionRange + "}";
     }
 }
 
-class World {
+class Cell {
+    enum Type {
+        Floor('.'),
+        Box('0');
+
+        char symbol;
+
+        Type(char symbol) {
+            this.symbol = symbol;
+        }
+    }
+
+    Type type = Type.Floor;
+    int utility = 0;
+
+    @Override
+    public String toString() {
+        return "" + type.symbol;
+    }
+}
+
+class Grid {
     int height;
     int width;
+    Cell cells[][];
+
+    void clear() {
+        cells = new Cell[width][height];
+    }
+
+    String showTypes() {
+        final StringBuilder sb = new StringBuilder(height * width);
+        for (int rowIndex = 0; rowIndex < height; ++rowIndex) {
+            for (int columnIndex = 0; columnIndex < width; ++columnIndex) {
+                sb.append(cells[columnIndex][rowIndex].type.symbol);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    String showUtility() {
+        final StringBuilder sb = new StringBuilder(height * width);
+        for (int rowIndex = 0; rowIndex < height; ++rowIndex) {
+            for (int columnIndex = 0; columnIndex < width; ++columnIndex) {
+                sb.append(cells[columnIndex][rowIndex].utility);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
+
+class World {
+    Grid grid;
     Boomer player = new Boomer();
     Boomer enemy = new Boomer();
     Bomb playerBomb;
@@ -35,10 +89,7 @@ class World {
 }
 
 // main class must be Player
-public class Player {
-
-    private static final char FLOOR = '.';
-    private static final char BOX = '0';
+class Player {
 
     private final Scanner in = new Scanner(System.in);
     private final World world = new World();
@@ -54,6 +105,10 @@ public class Player {
         while (true) {
             updateWorldState();
             in.nextLine();
+            calculateCellsWeight();
+
+            System.err.println(world.grid.showTypes());
+            System.err.println(world.grid.showUtility());
             System.err.println(world.player);
             System.err.println(world.enemy);
             System.err.println(world.playerBomb);
@@ -67,8 +122,10 @@ public class Player {
     }
 
     void initWorld() {
-        world.width = in.nextInt();
-        world.height = in.nextInt();
+        world.grid = new Grid();
+        world.grid.width = in.nextInt();
+        world.grid.height = in.nextInt();
+        world.grid.clear();
         world.player.id = in.nextInt();
         in.nextLine();
     }
@@ -76,9 +133,17 @@ public class Player {
     void updateWorldState() {
         world.playerBomb = null;
         world.enemyBomb = null;
-        for (int i = 0; i < world.height; i++) {
-            String row = in.nextLine();
-            System.err.println(row);
+        world.grid.clear();
+        for (int rowIndex = 0; rowIndex < world.grid.height; rowIndex++) {
+            final String row = in.nextLine();
+            for (int columnIndex = 0; columnIndex < world.grid.width; ++columnIndex) {
+                final Cell cell = new Cell();
+                final char typeSymbol = row.charAt(columnIndex);
+                if (typeSymbol == Cell.Type.Box.symbol) {
+                    cell.type = Cell.Type.Box;
+                }
+                world.grid.cells[columnIndex][rowIndex] = cell;
+            }
         }
         int entities = in.nextInt();
         for (int i = 0; i < entities; i++) {
@@ -110,6 +175,51 @@ public class Player {
                     break;
                 default:
                     break;
+            }
+        }
+    }
+
+    void calculateCellsWeight() {
+        final int width = world.grid.width;
+        final int height = world.grid.height;
+        final int bombRange = world.player.explosionRange - 1;
+        for (int columnIndex = 0; columnIndex < width; ++columnIndex) {
+            for (int rowIndex = 0; rowIndex < height; ++rowIndex) {
+                final Cell cell = world.grid.cells[columnIndex][rowIndex];
+                if (cell.type != Cell.Type.Floor) {
+                    // TODO: process non floor cells
+                    continue;
+                }
+                final int explosionColumnLeft = Math.max(columnIndex - bombRange, 0);
+                final int explosionColumnRight = Math.min(columnIndex + bombRange, width - 1);
+                final int explosionRowTop = Math.max(rowIndex - bombRange, 0);
+                final int explosionRowBottom = Math.min(rowIndex + bombRange, height - 1);
+                int crashedBoxCount = 0;
+                for (int explosionColumnIndex = columnIndex - 1; explosionColumnIndex >= explosionColumnLeft; --explosionColumnIndex) {
+                    if (world.grid.cells[explosionColumnIndex][rowIndex].type == Cell.Type.Box) {
+                        ++crashedBoxCount;
+                        break;
+                    }
+                }
+                for (int explosionColumnIndex = columnIndex + 1; explosionColumnIndex <= explosionColumnRight; ++explosionColumnIndex) {
+                    if (world.grid.cells[explosionColumnIndex][rowIndex].type == Cell.Type.Box) {
+                        ++crashedBoxCount;
+                        break;
+                    }
+                }
+                for (int explosionRowIndex = rowIndex - 1; explosionRowIndex >= explosionRowTop; --explosionRowIndex) {
+                    if (world.grid.cells[columnIndex][explosionRowIndex].type == Cell.Type.Box) {
+                        ++crashedBoxCount;
+                        break;
+                    }
+                }
+                for (int explosionRowIndex = rowIndex + 1; explosionRowIndex <= explosionRowBottom; ++explosionRowIndex) {
+                    if (world.grid.cells[columnIndex][explosionRowIndex].type == Cell.Type.Box) {
+                        ++crashedBoxCount;
+                        break;
+                    }
+                }
+                cell.utility = crashedBoxCount;
             }
         }
     }
